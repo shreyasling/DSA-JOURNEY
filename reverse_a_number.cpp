18bd36d9a53761990cc3b0cb00cd1c7856338b7f
shreyasling <shreyasling007@gmail.com>
2024-05-04 11:14:44 +0530

    MODIFIED CODE

--- reverse_a_number.cpp
+++ reverse_a_number.cpp
@@ -5,13 +5,17 @@
 
     int main()
     {   int a;
+        int rev=0;
         cin >> a; 
-        while(a>0){
+        while(a!=0){
             int digit=a%10;
-            cout << digit;
+            rev=rev*10+digit;
             
             a/=10;
+            
         }
+        cout << rev;
+        
 
         return 0;
     }
