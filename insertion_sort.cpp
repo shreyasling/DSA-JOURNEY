8149f7698507d74472eb76595a5099c1d85af45d
shreyasling <shreyasling007@gmail.com>
2024-05-04 21:57:34 +0530

    O(N)

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,21 +1,18 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 void swap(int a,int b){
     
 }
-void bubble_sort(int arr[],int n){
+void insertion_sirt(int arr[],int n){
     for(int i=n-1;i>=0;i--){
-        int count=0;
+        
         for(int j=0;j<=i-1;j++){
             if(arr[j]>arr[j+1]){
                  int temp=arr[j];
             arr[j]=arr[j+1];
             arr[j+1]=temp;
-            count++;
             }
         }
-        if(count ==0) break;
-        
        
     }
 }
@@ -28,7 +25,7 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    bubble_sort(arr,n);
+    inserion_sort(arr,n);
     for(int i=0;i<n;i++){
         cout << arr[i]<<" ";
     }
