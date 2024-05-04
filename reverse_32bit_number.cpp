8b8e22a206a42639e4f9892937143d855312f262
shreyasling <shreyasling007@gmail.com>
2024-05-04 12:27:46 +0530

    unsigned32bit

--- reverse_32bit_number.cpp
+++ reverse_32bit_number.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int reversebit(int n){
-    int reverse=0;
+    uint32_t reverse=0;
     for(int i=0;i<32;i++){
         reverse=(reverse <<1) | (n&1);
         n>>=1;
