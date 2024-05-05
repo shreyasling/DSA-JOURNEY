5baf792f063b77f69dd385a84e68bf24c367cb63
shreyasling <shreyasling007@gmail.com>
2024-05-05 20:53:50 +0530

    modifeid one

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -3,10 +3,9 @@ using namespace std;
 
 void merge(vector<int> &arr, int low, int mid, int high) {
     vector<int> temp; // temporary array
-    int left = low;      // starting index of left half of arr
-    int right = mid + 1;   // starting index of right half of arr
+    int left = low;   
+    int right = mid + 1;   
 
-    //storing elements in the temporary array in a sorted manner//
 
     while (left <= mid && right <= high) {
         if (arr[left] <= arr[right]) {
@@ -19,20 +18,20 @@ void merge(vector<int> &arr, int low, int mid, int high) {
         }
     }
 
-    // if elements on the left half are still left //
+   
 
     while (left <= mid) {
         temp.push_back(arr[left]);
         left++;
     }
 
-    //  if elements on the right half are still left //
+    
     while (right <= high) {
         temp.push_back(arr[right]);
         right++;
     }
 
-    // transfering all elements from temporary to arr //
+    
     for (int i = low; i <= high; i++) {
         arr[i] = temp[i - low];
     }
@@ -41,9 +40,9 @@ void merge(vector<int> &arr, int low, int mid, int high) {
 void mergeSort(vector<int> &arr, int low, int high) {
     if (low >= high) return;
     int mid = (low + high) / 2 ;
-    mergeSort(arr, low, mid);  // left half
-    mergeSort(arr, mid + 1, high); // right half
-    merge(arr, low, mid, high);  // merging sorted halves
+    mergeSort(arr, low, mid);  
+    mergeSort(arr, mid + 1, high); 
+    merge(arr, low, mid, high);
 }
 
 int main() {
