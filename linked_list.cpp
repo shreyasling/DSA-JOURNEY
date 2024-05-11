0339601ea03d7059b4c9bf7ec354fcb208df4760
shreyasling <shreyasling007@gmail.com>
2024-05-11 17:26:23 +0530

    convert array to linked list

---
+++ linked_list.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1,Node* next1){
+        data=data1;
+        next=next1;
+    }
+};
+
+int main(){
+    vector<int> arr ={0,2,3,1};
+    Node* y=new Node(arr[1],nullptr);
+    cout << y->data;
+}
